0149c2834941182ed15af2325915838dceea3053
eisBlume0901 <anecito.marykate09@gmail.com>
2026-02-04 17:08:42 +0800

    Created a report folder and files-related. Explored how routing works with Expo

--- CarboTrackr/app/report/index.tsx
+++ CarboTrackr/app/report/index.tsx
@@ -1,9 +1,17 @@
 import {  StyleSheet, Text, View } from "react-native";
+import { router} from "expo-router";
+import { Button } from "../../shared/components/Button";
+import {gradient} from "../../shared/constants/colors";
 
 export default function Index() {
     return (
         <View style={styles.container}>
             <Text>Indext</Text>
+            <Button
+                title="Go to Calorie Report Screen"
+                onPress={() => router.push("/report/calorie-report")}
+                gradient={gradient.green as [string, string]}
+            />
         </View>
     );
 }
