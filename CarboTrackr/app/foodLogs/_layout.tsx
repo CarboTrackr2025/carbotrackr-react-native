26b26a4a2e50d73107da7460a42c35a8df475955
eisBlume0901 <anecito.marykate09@gmail.com>
2026-02-10 13:30:53 +0800

    Added [food_id].tsx to the Stack.Screen

--- CarboTrackr/app/foodLogs/_layout.tsx
+++ CarboTrackr/app/foodLogs/_layout.tsx
@@ -3,7 +3,18 @@ import { Stack } from "expo-router";
 export default function Layout() {
     return (
         <Stack>
-            <Stack.Screen name="index" options={{headerShown: false}} />
+            <Stack.Screen
+                name="index"
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="[food_id]"
+                options={{
+                    headerShown: true,
+                    title: "Food Details",
+                }}
+            />
         </Stack>
-    )
+    );
 }
