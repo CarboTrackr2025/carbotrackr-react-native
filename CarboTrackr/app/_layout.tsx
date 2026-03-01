54e168ce6db5ecec9518714a7dfd65faed72e51b
alekzene <alekzene.cs@gmail.com>
2026-03-01 17:38:26 +0800

    register forgot password route

--- CarboTrackr/app/_layout.tsx
+++ CarboTrackr/app/_layout.tsx
@@ -2,25 +2,23 @@ import { Stack } from "expo-router"
 
 export default function RootLayout() {
     return (
-        <Stack screenOptions={{ headerShown: false }}>
+ <Stack screenOptions={{ headerShown: false }}>
             <Stack.Screen name="index" />
-            <Stack.Screen 
-                name="(tabs)" 
-                options={{
-                    gestureEnabled: false,
-                }}
+            <Stack.Screen
+                name="(tabs)"
+                options={{ gestureEnabled: false }}
             />
-            <Stack.Screen 
-                name="auth/login" 
-                options={{
-                    gestureEnabled: false,
-                }}
+            <Stack.Screen
+                name="auth/login"
+                options={{ gestureEnabled: false }}
             />
-            <Stack.Screen 
-                name="auth/signup" 
-                options={{
-                    gestureEnabled: false,
-                }}
+            <Stack.Screen
+                name="auth/signup"
+                options={{ gestureEnabled: false }}
+            />
+            <Stack.Screen
+                name="auth/forgot-password"
+                options={{ gestureEnabled: false }}
             />
         </Stack>
     )
